1dfdb0b550f29dae9c7e4a7a1aaee60198b785a0
Pamesh Bansal <pamesh12@gmail.com>
2019-12-28 21:19:40 +0530

    Completed code for using 3rd party OAUth server for Auth & Access Token

--- 3rdPartyOAuthAPI/apiproxy/resources/jsc/AccessTokenResponse.js
+++ 3rdPartyOAuthAPI/apiproxy/resources/jsc/AccessTokenResponse.js
@@ -1,3 +1,4 @@
-var tokenResponse = context.getVariable("response.content");
-var formattedResponse = JSON.stringify(JSON.parse(tokenResponse), null,2);
+var tokenResponse = context.getVariable("external.payload");
+var tokenJson = JSON.parse(tokenResponse);
+var formattedResponse = JSON.stringify(tokenJson, null,2);
 context.setVariable("response.content",formattedResponse);
